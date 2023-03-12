4be9e9c0824a3c6d8cfa8034321105dddbebda23
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2023-03-12 16:22:53 -0500

    add keyboard inputs

--- game.js
+++ game.js
@@ -7,6 +7,26 @@ window.addEventListener('load', function() {
 
     class InputHandler {
 
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if ((   (e.key === 'ArrowUp') ||
+                        (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                } else if ( e.key === ' '){
+                    this.game.player.shootTop();
+                } else if ( e.key === 'd'){
+                    this.game.debug = !this.game.debug;
+                }
+            });
+            window.addEventListener('keyup', e =>{
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+        
     }
 
     class Projectile {
@@ -22,9 +42,13 @@ window.addEventListener('load', function() {
             this.x = 20;
             this.y = 100;
             this.speedY = 0;
+            this.maxSpeed = 3;
         }
 
         update() {
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
             this.y += this.speedY;
         }
 
@@ -55,6 +79,8 @@ window.addEventListener('load', function() {
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
         }
 
         update() {
