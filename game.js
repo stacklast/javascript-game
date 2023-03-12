57c919349c40e4457980319da33b57d164e1f770
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2023-03-12 15:52:13 -0500

    Add player and game objects

--- game.js
+++ game.js
@@ -14,7 +14,23 @@ window.addEventListener('load', ()=>{
     }
 
     class Player {
-        
+
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+
+        update() {
+            this.y += this.speedY;
+        }
+
+        draw(context) { 
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     
     class Enemy {
@@ -35,5 +51,20 @@ window.addEventListener('load', ()=>{
 
     class Game {
 
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+
+        update() {
+            this.player.update();
+        }
+
+        draw(context) {
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
 });
