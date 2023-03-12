2de971c16e27b6981409f03fa05b5576ed2f84f7
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2023-03-12 15:35:46 -0500

    add canvas basic javascript setup

---
+++ game.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+    // canvas setup
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+});
